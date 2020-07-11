e22e956820cf42d8773abb5ac601610b76c36c48
Joseph <jtawil2015@gmail.com>
2020-07-11 12:10:31 -0800

    added horoscope card to members page

--- public/js/members.js
+++ public/js/members.js
@@ -4,4 +4,23 @@ $(document).ready(function() {
   $.get("/api/user_data").then(function(data) {
     $(".member-name").text(data.email);
   });
+
+  $.ajax({
+    type:"GET",
+    url:`http://sandipbgt.com/theastrologer/api/horoscope/leo/today`,
+    dataType:"json"
+  }).then( (res) =>
+  {
+    console.log(res.horoscope);
+    const horoscope = res.horoscope;
+    $(".container").append(`
+    <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
+     <div class="card-header">Header</div>
+      <div class="card-body">
+        <h5 class="card-title">Dark card title</h5>
+         <p class="card-text">${horoscope}</p>
+    </div>
+</div>`);
+  });
 });
+
