a558b6784ab6e2c93dad9601fc34e6f31ed396ce
Danielle Arias <daniellearias321@gmail.com>
2020-07-13 21:12:20 -0700

    fixed navbar responsiveness

--- public/js/members.js
+++ public/js/members.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+  // mobile responive nav for materialize
+  $(".sidenav").sidenav();
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(function (data) {
