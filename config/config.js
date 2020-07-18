53da6b7a44d4623e98dcc4aa0be8a5158609205e
John Merritt <johnmerritt95@gmail.com>
2020-07-18 12:13:13 -0700

    developmet branch will be used to deploy to heroku

---
+++ config/config.js
@@ -0,0 +1,23 @@
+require("dotenv").config(); // this is important!
+module.exports = {
+  development: {
+    username: process.env.DEV_DB_USERNAME,
+    password: process.env.DEV_DB_PASSWORD,
+    database: process.env.DEV_DB_DATABASE,
+    dialect: "mysql",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: process.env.PROD_DB_USERNAME,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_NAME,
+    host: process.env.PROD_DB_HOSTNAME,
+    dialect: "mysql",
+  },
+};
